c67ae0029f206b2b42065fe48544f593513e3ffb
Nicholas Tanryo <nick37tanryo@gmail.com>
2021-01-07 16:07:51 +0800

    public folder

---
+++ public/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+    });
+
+    $(".background-image").fadeIn(2000);
+
+    // slide-up script
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $("html").css("scrollBehavior", "auto");
+    });
+
+    $(".navbar .menu li a").click(function () {
+        // applying again smooth scroll on menu items click
+        $("html").css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", async function (event) {
+    // typing text animation script
+    try {
+        var typed = await new Typed(".typing", {
+            strings: ["Developer", "Student"],
+            typeSpeed: 120,
+            backSpeed: 100,
+            loop: true,
+        });
+    } catch (e) {}
+});
