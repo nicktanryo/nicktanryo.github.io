52f4b4c5ff75de79451f0dfc34585c707d365c70
Nicholas Tanryo <nick37tanryo@gmail.com>
2021-01-25 21:43:35 +0800

    change cv directory

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,58 +20,32 @@ export default function Header({ highlighted }) {
             <div className="max-width">
                 <div>
                     {highlighted === PAGES.ABOUT && (
-                        <a
-                            className="download-cv"
-                            href="/assets/Nicholas_Tanryo_CV.pdf"
-                            download
-                        >
+                        <a className="download-cv" href="/assets/NicholasTanryo.pdf" download>
                             Download CV
                         </a>
                     )}
                 </div>
                 <ul className="menu">
                     <li>
-                        <a
-                            href="/#/home"
-                            className={
-                                "menu-btn " +
-                                (highlighted === PAGES.HOME && "highlighted")
-                            }
-                        >
+                        <a href="/#/home" className={"menu-btn " + (highlighted === PAGES.HOME && "highlighted")}>
                             Home
                         </a>
                     </li>
                     <li>
-                        <a
-                            href="/#/about"
-                            className={
-                                "menu-btn " +
-                                (highlighted === PAGES.ABOUT && "highlighted")
-                            }
-                        >
+                        <a href="/#/about" className={"menu-btn " + (highlighted === PAGES.ABOUT && "highlighted")}>
                             About
                         </a>
                     </li>
                     <li>
                         <a
                             href="/#/projects"
-                            className={
-                                "menu-btn " +
-                                (highlighted === PAGES.PROJECTS &&
-                                    "highlighted")
-                            }
+                            className={"menu-btn " + (highlighted === PAGES.PROJECTS && "highlighted")}
                         >
                             Projects
                         </a>
                     </li>
                     <li>
-                        <a
-                            href="/#/contact"
-                            className={
-                                "menu-btn " +
-                                (highlighted === PAGES.CONTACT && "highlighted")
-                            }
-                        >
+                        <a href="/#/contact" className={"menu-btn " + (highlighted === PAGES.CONTACT && "highlighted")}>
                             Contact
                         </a>
                     </li>
